b386cf57c0d5306e7aae4cc3783c997c093239b7
shatha <shatha200>
2024-06-13 20:09:12 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+N = int(input("Entrer le nombre des queens"))
+board = [[0]*N for i in range(N)]
+
+def attack(i, j):
+    #verticale & horizontale
+    for k in range(0,N):
+        if board[i][k]==1 or board[k][j]==1:
+            return True
+    # diagonal
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if board[k][l]==1:
+                    return True
+    return False
+
+def N_queens(n):
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            if (not(attack(i,j))) and (board[i][j]!=1):
+                board[i][j] = 1
+                if N_queens(n-1)==True:
+                    return True
+                board[i][j] = 0
+
+    return False
+
+N_queens(N)
+for i in board:
+    print (i)
